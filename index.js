59adadaed4b46a626b36d5dd25dc6d95b9529821
Elias Ojala <ojala.s.elias@gmail.com>
2017-12-01 22:27:40 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+app.disable('x-powered-by');
+
+app.use((req, res) => {
+    res.set('Content-Type', 'application/json');
+    res.send(JSON.stringify(req.headers, null, 2));
+});
+
+if (app.get('env') !== 'test') {
+    const port = process.env.PORT || 5000;
+  
+    app.listen(port, () => {
+      console.log('Listening on port ' + port);
+    });
+  }
